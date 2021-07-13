7233a989d9a7f8d173505bfa7babebda4b49c3df
ignotochi <lorenzo@fetchtec.com>
2021-07-13 16:06:21 +0200

    New Files

---
+++ src/app/components/preview/stream-preview.component.ts
@@ -0,0 +1,44 @@
+import { AfterViewInit, Component, ElementRef, Injectable, Input, OnInit, QueryList, ViewChild, ViewChildren } from "@angular/core";
+import { SharedService } from "src/app/services/shared.service";
+
+@Injectable()
+
+@Component({
+    selector: 'stream-preview',
+    templateUrl: 'stream-preview.component.html',
+    styleUrls: ['./stream-preview.component.scss'],
+  })
+  export class StreamPreview implements OnInit, AfterViewInit {
+    @ViewChild('streaming', { static: false }) streaming: ElementRef<HTMLImageElement>;
+    @ViewChild('spinner', { read: ElementRef }) spinner: ElementRef<HTMLElement>;
+    streamUrl: string;
+
+    constructor(private sharedService: SharedService) {
+      this.streamUrl = this.sharedService.streamUrl;
+    }
+
+    ngOnInit() {
+    }
+
+    ngAfterViewInit() {
+    }
+
+    stopStreaming(): void {
+      this.streaming.nativeElement.src = null;
+    }
+
+    alignSpinners(loadStatus: boolean) {
+      if (loadStatus === true) { this.spinner.nativeElement.classList.add('hidden'); } 
+    }
+
+    onImageLoad(evt: any) {
+      if (evt && evt.target) {
+        const width = evt.target.naturalWidth;
+        const height = evt.target.naturalHeight;
+        const status = evt.target.complete;
+        const isLoaded = (width !== 0 && height !== 0 && status === true) ? true : false;
+        this.alignSpinners(isLoaded);
+      }
+    }
+
+  }
